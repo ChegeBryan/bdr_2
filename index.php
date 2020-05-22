018c98af3d9231fe9db4aca9e40d702b9cbc0b8d
Chege Bryan <cbbryan8@gmail.com>
2020-05-22 10:32:06 +0300

    🎨 Add card description for user activity

--- index.php
+++ index.php
@@ -45,6 +45,41 @@
       </div>
     </nav>
 
+    <div class="container d-flex flex-column flex-md-row justify-content-around py-4">
+      <div class="card shadow-lg rounded-lg ml-2 mb-2" style="width: 18rem;">
+        <i class="fa fa-5x fa-h-square text-center text-secondary py-4"></i>
+        <div class="card-body">
+          <h5 class="card-title text-center">Hospital</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Functions</h6>
+          <p class="card-text">View user health history</p>
+          <p class="card-text">Edit user health record</p>
+          <p class="card-text">Add user health</p>
+          <a href="#" class="btn btn-secondary stretched-link">Dashboard</a>
+        </div>
+      </div>
+      <div class="card shadow-lg rounded-lg ml-2 mb-2" style="width: 18rem;">
+        <i class="fa fa-mortar-board fa-5x text-center text-secondary py-4"></i>
+        <div class="card-body">
+          <h5 class="card-title text-center">School</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Functions</h6>
+          <p class="card-text py-2">View user academics</p>
+          <p class="card-text py-2">Add user academics</p>
+          <a href="#" class="btn btn-secondary stretched-link">Dashboard</a>
+        </div>
+      </div>
+      <div class="card shadow-lg rounded-lg ml-2 mb-2" style="width: 18rem;">
+        <i class="fa fa-suitcase fa-5x text-center text-secondary py-4"></i>
+        <div class="card-body ">
+          <h5 class="card-title text-center">Company</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Functions</h6>
+          <p class="card-text">View user work history</p>
+          <p class="card-text">Add user work details</p>
+          <p class="card-text">View user academics</p>
+          <a href="#" class="btn btn-secondary stretched-link">Dashboard</a>
+        </div>
+      </div>
+    </div>
+
 
     <script src="assets/js/jquery.min.js"></script>
 
