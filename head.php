61be82cd658a8c6bf00e02cc9b0e7e75c45b2dcc
Chege Bryan <cbbryan8@gmail.com>
2020-05-22 18:56:09 +0300

    🍱 Add admin login page

---
+++ head.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗂</text></svg>">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="../assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/font-awesome-4.7.0/css/font-awesome.css">
