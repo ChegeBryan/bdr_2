0d8a44d914f78699c8bea222ed332708a5c39eb6
Chege Bryan <cbbryan8@gmail.com>
2020-05-22 10:30:42 +0300

    🔗 Add Homepage navbar

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗂</text></svg>">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="assets/bootstrap-4.5.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.css">
+    <title>Biodata Records System</title>
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-bg bg-secondary">
+
+
+      <a class="navbar-brand text-white" href="index.php">Biodata Records</a>
+
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon text-white"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="hospital/login.php">Hospital</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="school/login.php">School</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="company/login.php">Company</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="user/login.php">User</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="admin/login.php">Admin</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
