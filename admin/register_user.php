266718ca4e4aaeb4c51aaaccb7f1f41302fb08c0
Chege Bryan <cbbryan8@gmail.com>
2020-05-24 09:58:20 +0300

    🔗 Update home link to point to admin dashoard

--- admin/register_user.php
+++ admin/register_user.php
@@ -65,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           <a class="nav-link text-white" href="logout.php">Logout</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="../index.php">Home</a>
+          <a class="nav-link text-white" href="dashboard.php">Home</a>
         </li>
       </ul>
     </nav>
