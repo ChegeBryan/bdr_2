d2fff1c700f919b1eb37a49f172a469883ada11e
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 18:53:26 +0300

    🗃  Get user health details from the database

--- hospital/view_usr.php
+++ hospital/view_usr.php
@@ -86,6 +86,58 @@ require_once '../includes/config.php';
       </div>
     </div>
 
+    <div class="container" style="width: 1024px">
+      <div class="table-responsive">
+        <table class="table table-striped table-hover table-sm">
+          <thead class="text-secondary">
+            <tr>
+              <th scope="col">Entered on</th>
+              <th scope="col">Hospital Visited</th>
+              <th scope="col">Diagnosis</th>
+              <th scope="col">Medication</th>
+              <th scope="col">Healed</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+          $sql = "SELECT entered_on, name, diagnosis, medication, healed
+                    FROM bdr_health_information
+                    JOIN bdr_hospital
+                    ON bdr_health_information.hospital=bdr_hospital.id
+                    WHERE bdr_health_information.user= ?
+                    ORDER BY bdr_health_information.id DESC";
+
+          if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param("i", $user);
+
+            $user = $_GET["user"];
+
+            if ($stmt->execute()) {
+              $result = $stmt->get_result();
+
+              if ($result->num_rows > 0) {
+                while ($row = $result->fetch_array()) {
+                  echo "<tr>";
+                  echo "<td>" . date_format(date_create($row['entered_on']), "d-M-Y") . "</td>";
+                  echo "<td>" . $row["name"] . "</td>";
+                  echo "<td>" . $row["diagnosis"] . "</td>";
+                  echo "<td>" . $row['medication'] . "</td>";
+                  echo "<td>" . ($row['healed'] == 0 ? "No" : "Yes") . "</td>";
+                  echo "</tr>";
+                }
+              } else {
+                echo "<tr><td colspan='5'>No health information entered yet.</td></tr>";
+              }
+            }
+            $stmt->close();
+          }
+          ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
     <script src="../assets/js/jquery.min.js"></script>
 
     <script src="../assets/js/popper.min.js"></script>
