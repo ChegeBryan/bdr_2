ebb0d7f284f7952fd7d618b34aec9c06d15de9d9
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 10:21:10 +0300

    📁 Verify school logins against db

--- school/login.php
+++ school/login.php
@@ -1,3 +1,66 @@
+<?php
+
+session_start();
+
+require_once "../includes/config.php";
+
+$sch_id = $password = "";
+$sch_id_err = $password_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty(trim($_POST["sch_id"]))) {
+    $sch_id_err = "Please enter school id.";
+  } else {
+    $sch_id = trim($_POST["sch_id"]);
+  }
+
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter your password.";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  if (empty($h_id_err) && empty($password_err)) {
+    $sql = "SELECT id, sch_id, password
+    FROM bdr_school
+    WHERE sch_id = ?";
+
+    if ($stmt = $conn->prepare($sql)) {
+
+      $stmt->bind_param("s", $identity);
+      $identity = $sch_id;
+      if ($stmt->execute()) {
+        $stmt->store_result();
+
+        if ($stmt->num_rows == 1) {
+          $stmt->bind_result($id, $sch_id, $hashed_password);
+
+          if ($stmt->fetch()) {
+            if (password_verify($password, $hashed_password)) {
+
+              $_SESSION["school_logged_in"] = true;
+              $_SESSION["sch_id"] = $id;
+              $_SESSION["sch"] = $sch_id;
+
+              header("location: dashboard.php?school=" . $_SESSION['sch_id']);
+            } else {
+              $password_err = "The password you entered was not valid.";
+            }
+          }
+        } else {
+          $h_id_err = "No account found with that school id found.";
+        }
+      } else {
+        header("location: ../error.php");
+      }
+      $stmt->close();
+    }
+  }
+  $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
