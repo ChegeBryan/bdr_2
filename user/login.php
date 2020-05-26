03e79bef139be5a08f69a8530ffbb416e62637e7
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 10:54:37 +0300

    📁 Verify user login details against the db

--- user/login.php
+++ user/login.php
@@ -1,3 +1,71 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_logged_in"]) && $_SESSION["user_logged_in"] === true) {
+  header("location: dashboard.php?user=" . $_SESSION['user_id']);
+  exit;
+}
+
+require_once "../includes/config.php";
+
+$user_id = $password = "";
+$user_id_err = $password_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty(trim($_POST["user_id"]))) {
+    $user_id_err = "Please enter user id.";
+  } else {
+    $user_id = trim($_POST["user_id"]);
+  }
+
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter your password.";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  if (empty($comp_id_err) && empty($password_err)) {
+    $sql = "SELECT id, userId, password
+    FROM bdr_users
+    WHERE user_id = ?";
+
+    if ($stmt = $conn->prepare($sql)) {
+
+      $stmt->bind_param("s", $identity);
+      $identity = $user_id;
+      if ($stmt->execute()) {
+        $stmt->store_result();
+
+        if ($stmt->num_rows == 1) {
+          $stmt->bind_result($id, $user_id, $hashed_password);
+
+          if ($stmt->fetch()) {
+            if (password_verify($password, $hashed_password)) {
+
+              $_SESSION["user_logged_in"] = true;
+              $_SESSION["user_id"] = $id;
+              $_SESSION["user"] = $user_id;
+
+              header("location: dashboard.php?user=" . $_SESSION['user_id']);
+            } else {
+              $password_err = "The password you entered was not valid.";
+            }
+          }
+        } else {
+          $user_id_err = "No account found with that company id found.";
+        }
+      } else {
+        header("location: ../error.php");
+      }
+      $stmt->close();
+    }
+  }
+  $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
