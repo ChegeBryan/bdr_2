b0396e30c42dafb4d7e8f964cc69fd73670e4f0e
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 20:58:42 +0300

    🔗 Add logout link

--- hospital/navbar.php
+++ hospital/navbar.php
@@ -17,7 +17,7 @@
           users</a>
       </li>
       <li class=" nav-item">
-        <a class="nav-link text-white" href="#">Logout</a>
+        <a class="nav-link text-white" href="logout.php">Logout</a>
       </li>
     </ul>
   </div>
