eeafe2c6f75b738776d14bd193fff0c5237232b5
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 10:41:04 +0300

    📁 Verify company login against db

--- company/login.php
+++ company/login.php
@@ -1,3 +1,71 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["company_logged_in"]) && $_SESSION["company_logged_in"] === true) {
+  header("location: dashboard.php?company=" . $_SESSION['comp_id']);
+  exit;
+}
+
+require_once "../includes/config.php";
+
+$comp_id = $password = "";
+$comp_id_err = $password_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty(trim($_POST["comp_id"]))) {
+    $comp_id_err = "Please enter company id.";
+  } else {
+    $comp_id = trim($_POST["comp_id"]);
+  }
+
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter your password.";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  if (empty($comp_id_err) && empty($password_err)) {
+    $sql = "SELECT id, comp_id, password
+    FROM bdr_company
+    WHERE comp_id = ?";
+
+    if ($stmt = $conn->prepare($sql)) {
+
+      $stmt->bind_param("s", $identity);
+      $identity = $comp_id;
+      if ($stmt->execute()) {
+        $stmt->store_result();
+
+        if ($stmt->num_rows == 1) {
+          $stmt->bind_result($id, $comp_id, $hashed_password);
+
+          if ($stmt->fetch()) {
+            if (password_verify($password, $hashed_password)) {
+
+              $_SESSION["school_logged_in"] = true;
+              $_SESSION["comp_id"] = $id;
+              $_SESSION["comp"] = $comp_id;
+
+              header("location: dashboard.php?company=" . $_SESSION['comp_id']);
+            } else {
+              $password_err = "The password you entered was not valid.";
+            }
+          }
+        } else {
+          $comp_id_err = "No account found with that company id found.";
+        }
+      } else {
+        header("location: ../error.php");
+      }
+      $stmt->close();
+    }
+  }
+  $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
