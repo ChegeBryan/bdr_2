06efa265d696bce1aea231504dc75301199ad4f9
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 18:24:58 +0300

    ♻ Make user avatar responsive

--- hospital/view_usr.php
+++ hospital/view_usr.php
@@ -22,67 +22,66 @@ require_once '../includes/config.php';
     <?php include 'navbar.php'; ?>
 
     <div class="container py-3" style="width: 1024px">
-      <div class="row">
-        <?php
-      if (isset($_GET["user"])) {
-        $sql = "SELECT fullname, userid, health, pic, gender, dob FROM bdr_users WHERE id = ?";
-        if ($stmt = $conn->prepare($sql)) {
-          $stmt->bind_param("i", $id);
-          $id = trim($_GET["user"]);
-          if ($stmt->execute()) {
-            $result = $stmt->get_result();
 
-            if ($result->num_rows == 1) {
-              $row = $result->fetch_array(MYSQLI_ASSOC);
-            } else {
-              echo "Something went wrong.";
-              exit();
-            }
+      <?php
+    if (isset($_GET["user"])) {
+      $sql = "SELECT fullname, userid, health, pic, gender, dob FROM bdr_users WHERE id = ?";
+      if ($stmt = $conn->prepare($sql)) {
+        $stmt->bind_param("i", $id);
+        $id = trim($_GET["user"]);
+        if ($stmt->execute()) {
+          $result = $stmt->get_result();
+
+          if ($result->num_rows == 1) {
+            $row = $result->fetch_array(MYSQLI_ASSOC);
           } else {
-            header('location: ../error.php');
+            echo "Something went wrong.";
+            exit();
           }
+        } else {
+          header('location: ../error.php');
         }
       }
-      ?>
-        <div class="col-sm-12">
-          <h4 class="text-center">User Details</h4>
-          <div class="media">
-            <img src="<?php echo $row['pic']; ?>" class="align-self-center mr-3 img-thumbnail" alt="Profile Image">
-            <div class="media-body">
-              <div class="table-responsive">
-                <table class="table table-striped">
-                  <tr>
-                    <th>Full Name</th>
-                    <td><?php echo ucwords($row['fullname']); ?>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>User Identity</th>
-                    <td><?php echo $row['userid']; ?>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>Gender</th>
-                    <td><?php echo ucfirst($row['gender']); ?>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>Date of Birth</th>
-                    <td><?php echo $row['dob']; ?>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>Health</th>
-                    <td><?php echo $row['health']; ?>
-                    </td>
-                  </tr>
-                </table>
-              </div>
-              <a href="<?php echo 'add_health_info.php?hospital=' . $_GET['hospital'] . '&user=' . $_GET['user'] ?>"
-                 class="btn btn-info">Add
-                health information</a>
-            </div>
+    }
+    ?>
+      <h4 class="text-center">User Details</h4>
+      <div class="row">
+        <div class="col-sm-4">
+          <img src="<?php echo $row['pic']; ?>" class="img-fluid img-thumbnail" alt="Profile Image">
+        </div>
+        <div class="col-sm-8">
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <tr>
+                <th>Full Name</th>
+                <td><?php echo ucwords($row['fullname']); ?>
+                </td>
+              </tr>
+              <tr>
+                <th>User Identity</th>
+                <td><?php echo $row['userid']; ?>
+                </td>
+              </tr>
+              <tr>
+                <th>Gender</th>
+                <td><?php echo ucfirst($row['gender']); ?>
+                </td>
+              </tr>
+              <tr>
+                <th>Date of Birth</th>
+                <td><?php echo $row['dob']; ?>
+                </td>
+              </tr>
+              <tr>
+                <th>Health</th>
+                <td><?php echo $row['health']; ?>
+                </td>
+              </tr>
+            </table>
           </div>
+          <a href="<?php echo 'add_health_info.php?hospital=' . $_GET['hospital'] . '&user=' . $_GET['user'] ?>"
+             class="btn btn-info">Add
+            health information</a>
         </div>
       </div>
     </div>
