040c7a8274a0576a66336376f4f319434845277e
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 12:34:49 +0300

    🎨 Add input field to search for user

--- hospital/search_user.php
+++ hospital/search_user.php
@@ -19,6 +19,28 @@ if (isset($_SESSION["hospital_logged_In"]) || $_SESSION["hospital_logged_in"] !=
     <?php include 'navbar.php'; ?>
 
 
+    <div class="container py-3" style="width: 1024px">
+      <div class="row">
+        <div class="col-sm-4">
+          <form>
+            <label for="User_reg">User Registration number</label>
+            <div class="input-group mb-2 mr-sm-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-search"></i></div>
+              </div>
+              <input type="text" class="form-control" id="user_reg" name="user_reg" placeholder="Registration number">
+            </div>
+          </form>
+          <p class="text-muted">Enter user registration to search for the user in the users directory.</p>
+        </div>
+        <div class="col-sm-8">
+
+        </div>
+      </div>
+    </div>
+
+
+
     <script src="assets/js/jquery.min.js"></script>
 
     <script src="assets/js/popper.min.js"></script>
