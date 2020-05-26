7fffebefe9e03c61fad5bb4fed07f80e685d7510
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 12:15:01 +0300

    ♻ Move hospital navbar to indendent file

--- hospital/dashboard.php
+++ hospital/dashboard.php
@@ -16,29 +16,8 @@ if (isset($_SESSION["hospital_logged_In"]) || $_SESSION["hospital_logged_in"] !=
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-sm navbar-bg bg-secondary">
-      <a class="navbar-brand text-white" href="../index.php">Biodata Records</a>
 
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon text-white"></span>
-      </button>
-
-      <!-- Links -->
-      <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#">Search User</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#">View users</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <?php include 'navbar.php'; ?>
 
     <div class="container py-3" style="width: 1024px">
       <div class="row">
