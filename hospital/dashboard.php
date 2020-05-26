1592fa9d14b2e9a9f95386b4cea6aa6a9433ff36
Chege Bryan <cbbryan8@gmail.com>
2020-05-26 11:34:45 +0300

    🎨 Add hospital dashboard navbar

---
+++ hospital/dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include '../head.php'; ?>
+    <title>Hospital | Dashboard</title>
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-bg bg-secondary">
+      <a class="navbar-brand text-white" href="../index.php">Biodata Records</a>
+
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon text-white"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Search User</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">View users</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
