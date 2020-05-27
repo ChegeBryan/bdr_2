45f448745a1622634ac6a528a6734c250638d9fb
Chege Bryan <cbbryan8@gmail.com>
2020-05-27 19:00:37 +0300

    🗃  Query database for user list

---
+++ company/find_usr.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require_once '../includes/config.php';
+
+if (!empty($_POST["user_reg"])) {
+  $sql = "SELECT * FROM bdr_users WHERE userid LIKE '%{$_POST["user_reg"]}%'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      echo <<<ROW
+      <tr>
+        <td>{$row["userid"]}</td>
+        <td><img src='{$row["pic"]}' class='img-fluid' height='50px' width='50px'></td>
+        <td>{$row["fullname"]}</td>
+        <td>{$row["gender"]}</td>
+        <td><a href="view_usr.php?company={$_SESSION['comp_id']}&user={$row['id']}" class="btn btn-info btn-sm">View</a></td>
+      </tr>
+      ROW;
+    }
+  } else {
+    echo <<<ROW
+      <tr>
+        <td colspan='5' class="text-warning">No user found matching the registration number.</td>
+      </tr>
+      ROW;
+  }
+} else {
+  echo <<<ROW
+      <tr>
+        <td colspan='5' class='text-info'>Type into the search field to look for user.</td>
+      </tr>
+      ROW;
+}
