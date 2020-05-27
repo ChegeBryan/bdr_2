e1cb5e0b25591d86091abd718a6ac46b5c29b50f
Chege Bryan <cbbryan8@gmail.com>
2020-05-27 13:38:57 +0300

    🍱 Add school dashboard navigation bar

---
+++ school/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include '../head.php'; ?>
+    <title>School | Dashboard</title>
+  </head>
+
+  <body>
+
+    <?php include 'navbar.php'; ?>
+
+    <script src="assets/js/jquery.min.js"></script>
+
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
