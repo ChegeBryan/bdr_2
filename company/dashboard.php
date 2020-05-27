e630caf6a9932bda943ee8d3389edce27515d130
Chege Bryan <cbbryan8@gmail.com>
2020-05-27 18:49:10 +0300

    🎨 Add call to action cards

--- company/dashboard.php
+++ company/dashboard.php
@@ -20,6 +20,36 @@ if (isset($_SESSION["company_logged_In"]) || $_SESSION["company_logged_in"] !== 
     <?php include 'navbar.php'; ?>
 
 
+    <div class="container py-3" style="width: 1024px">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card shadow-lg rounded-lg mb-2">
+            <i class="fa fa-5x fa fa-search text-center text-primary py-4"></i>
+            <div class="card-body text-center">
+              <h4 class="card-title">User Directory</h4>
+              <h6 class="card-subtitle mb-2 text-muted font-weight-bold">Visit page to:</h6>
+              <p class="card-text">Search user using their registration number</p>
+              <p class="card-text">View user details</p>
+              <p class="card-text">Add work experience information</p>
+              <a href="#" class="btn btn-primary stretched-link btn-block">User Directory</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card shadow-lg rounded-lg mb-2">
+            <i class="fa fa-5x fa-briefcase text-center text-primary py-4"></i>
+            <div class="card-body text-center">
+              <h4 class="card-title">Company Directory</h4>
+              <h6 class="card-subtitle mb-2 text-muted font-weight-bold">Visit page to:</h6>
+              <p class="card-text">View registered users who have worked in this company before.</p>
+              <p class="card-text">View user details</p>
+              <a href="#" class="btn btn-primary stretched-link btn-block">View Users</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script src="assets/js/jquery.min.js"></script>
 
     <script src="assets/js/popper.min.js"></script>
