966c5450edd37b1cbc34acee847ba51b3a6af9cd
Chege Bryan <cbbryan8@gmail.com>
2020-05-27 13:49:19 +0300

    🔐 Restrict dashboard access when logged in only

--- school/dashboard.php
+++ school/dashboard.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if (isset($_SESSION["school_logged_In"]) || $_SESSION["school_logged_in"] !== true) {
+  header("location: login.php");
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
