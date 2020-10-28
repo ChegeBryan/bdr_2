df202212dd848df1b647f097b99d867d6582228e
Chege Bryan <cbbryan8@gmail.com>
2020-10-28 14:33:32 +0300

    Fix user search

--- hospital/find_usr.php
+++ hospital/find_usr.php
@@ -4,7 +4,7 @@ session_start();
 require_once '../includes/config.php';
 
 if (!empty($_POST["user_reg"])) {
-  $sql = "SELECT * FROM bdr_users WHERE userid LIKE '%{$_POST["user_reg"]}%'";
+  $sql = "SELECT * FROM bdr_users WHERE userid = '%{$_POST["user_reg"]}%'";
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
